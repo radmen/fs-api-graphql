8ec231f77f6a3bb0f52f1c6137330a9394a89f81
Radoslaw Mejer <radmen@desmart.com>
2018-03-12 13:00:39 +0100

    Add files

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+const { makeExecutableSchema } = require('graphql-tools')
+
+const app = express()
+app.use(bodyParser.json())
+
+const files = [
+    'foo.jpg',
+    'bar.jpg'
+]
+
+const typeDefs = `
+    type File { path: String! }
+
+    type Query {
+        list: [File]!
+    }
+`
+
+const resolvers = {
+    Query: {
+        list: () => files.map(path => ({ path }))
+    }
+}
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
+
+app.use('/graphql', graphqlExpress({ schema }))
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
+
+app.listen(3000, () => {
+  console.log('Go to http://localhost:3000/graphiql to run queries!');
+})
+
